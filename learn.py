a3ce1cde2e90f64b3aa09b6e3824aa20f7dd3d29
preacherabe <grabeguytech@gmail.com>
2022-09-20 12:04:21 +0100

    new line of code

--- learn.py
+++ learn.py
@@ -12,3 +12,26 @@ print("Name")
 print("I love you so much")
 print("get well soon honey")
 print("You are my honeypie")
+
+dogs = ["dollar", 1, True]
+
+print(dogs[2])
+
+dogs[2] = True
+
+print(dogs[-1])
+
+print(dogs[:2])
+
+
+print(len(dogs))
+
+dogs.append('Julius')
+
+print(dogs)
+
+dogs.append("kenny")
+
+print(dogs)
+
+dogs.extend(["Martins", 5, 6, 7])
